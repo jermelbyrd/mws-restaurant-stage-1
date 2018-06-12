ea14ab8caae8cae19c01475d5cc9aedb645ecc21
Jermel Byrd <jermelbyrd@gmail.com>
2018-06-12 12:32:15 -0400

    updated service worker file

--- js/sw.js
+++ js/sw.js
@@ -1,3 +1,23 @@
+event.waitUntil (
+  caches.open(staticCacheName).then(function(cache){
+    return cache.addAll([
+      'skeleton',
+      'js/main.js',
+      'css/main.css',
+      'img/icon.jpg']);
+    })
+  );
+
+self.addEventListener('fetch', function(event){
+  var requestURL = new URL(event.request.url);
+
+  if (requestURL.origin === location.origin) {
+    if (requestURL.pathname ==='/'){
+      event.respondWith(caches.match('/skeleton'));
+      return;
+    }
+  }
+})
 self.addEventListener('fetch', function(event) {
   event.respondWith(caches.match(event.request));
 });
